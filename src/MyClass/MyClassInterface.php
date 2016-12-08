64e61bf174d20a1368276d349b5dff31be4a6d9c
Alessandra Bilardi <alessandra.bilardi@gmail.com>
2016-12-08 02:50:18 +0100

    step 5 - add interface class and the bar function: phpunit works properly

--- src/MyClass/MyClassInterface.php
+++ src/MyClass/MyClassInterface.php
@@ -2,10 +2,8 @@
 
 namespace MyClass;
 
-use MyClass\MyClassInterface;
-
 /**
- * Class of an example
+ * Interface of MyClass
  *
  * An example of class that it extends an abstract class and it implements an interface.
  * There is a boolean pun by foo function of abstract class, bar function of interface class,
@@ -15,28 +13,12 @@ use MyClass\MyClassInterface;
  * @author Alessandra Bilardi <alessandra.bilardi@gmail.com>
  * @see https://github.com/bilardi/php-prototype for details
  */
-class MyClass implements MyClassInterface
+interface MyClassInterface
 {
-    protected $bar;
-
     /**
-     * Constructs the object
-     *
-     * @param boolean $foobar
-     * @return object
-     */
-    public function __construct($bar = true)
-    {
-        $this->bar = (bool)$bar;
-    }
-
-    /**
-     * Get bar value
+     * Bar
      *
      * @return boolean
      */
-    public function bar()
-    {
-        return $this->bar;
-    }
+    public function bar();
 }
