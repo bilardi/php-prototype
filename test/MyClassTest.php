511872d237e7e4d6f849de0c95d52cf486bd1859
Alessandra Bilardi <alessandra.bilardi@gmail.com>
2016-12-08 02:09:47 +0100

    step 2 - add the test class with the first test: if the class can be created; phpcs works properly

---
+++ test/MyClassTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MyClass\Test;
+
+use MyClass\MyClass;
+
+/**
+ * Test class of MyClass
+ *
+ * This is a basic unit test class. There is a test for each public function.
+ * If the functions contained conditions, there would be more tests for each public function.
+ *
+ * @license MIT
+ * @author Alessandra Bilardi <alessandra.bilardi@gmail.com>
+ * @see https://github.com/bilardi/php-prototype for details
+ */
+class MyClassTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Verifies if the class MyClass can be created
+     */
+    public function testMyClassCanBeCreated()
+    {
+        $myClass = new MyClass();
+    }
+}
